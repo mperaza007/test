cb0e25f7d0723a6272815fd158e6b4d959004841
mperaza007 <mperazaravelo@mail.valenciacollege.edu>
2026-02-27 15:39:37 -0500

    Initial commit

---
+++ src/test/java/myGroup/AppTest.java
@@ -0,0 +1,13 @@
+package myGroup;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class AppTest {
+    @Test
+    void testAdd(){
+        assertEquals(5, App.add(2,3));
+    }
+
+}
