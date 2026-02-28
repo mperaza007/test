c0aae317a0ea768672601fb162d62447e58c41d7
mperaza007 <mperazaravelo@mail.valenciacollege.edu>
2026-02-28 13:09:00 -0500

    Modified all the files to put the test-pipeline-jenkins in lowercase

--- src/main/java/myGroup/App.java
+++ src/main/java/myGroup/App.java
@@ -6,6 +6,6 @@ public class App {
     }
 
     public static void main(String[] args) {
-            System.out.println("App running...");
+            System.out.println("App running....");
     }
 }
